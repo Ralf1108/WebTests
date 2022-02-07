229c19f8f0ef1c118d6b5247c21cda2097a96c5b
Ralf1108 <ralf.kornelius@gmx.de>
2022-02-07 17:18:30 +0100

    added skia sample code

---
+++ BlazorSkiaWebAssemblyPerformanceTester/BlazorSkiaWebAssemblyPerformanceTester/Models/Ball.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCanvasTest2.Models
+{
+    public class Ball
+    {
+        public float X { get; private set; }
+        public float Y { get; private set; }
+        public float XVel { get; private set; }
+        public float YVel { get; private set; }
+        public float R { get; private set; }
+        public string Color { get; private set; }
+
+        public Ball(float x, float y, float xVel, float yVel, float radius, string color)
+        {
+            (X, Y, XVel, YVel, R, Color) = (x, y, xVel, yVel, radius, color);
+        }
+
+        public void StepForward(float width, float height)
+        {
+            X += XVel;
+            Y += YVel;
+            if (X < 0 || X > width)
+                XVel *= -1;
+            if (Y < 0 || Y > height)
+                YVel *= -1;
+
+            if (X < 0)
+                X += 0 - X;
+            else if (X > width)
+                X -= X - width;
+
+            if (Y < 0)
+                Y += 0 - Y;
+            if (Y > height)
+                Y -= Y - height;
+        }
+    }
+}
---
+++ BlazorSkiaWebAssemblyPerformanceTester/BlazorSkiaWebAssemblyPerformanceTester/Models/Field.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCanvasTest2.Models
+{
+    public class Field
+    {
+        public readonly List<Ball> Balls = new List<Ball>();
+        public float Width { get; private set; }
+        public float Height { get; private set; }
+
+        public void Resize(float width, float height) =>
+            (Width, Height) = (width, height);
+
+        public void StepForward()
+        {
+            foreach (Ball ball in Balls)
+                ball.StepForward(Width, Height);
+        }
+
+        private float RandomVelocity(Random rand, float min, float max)
+        {
+            float v = min + (max - min) * (float)rand.NextDouble();
+            if (rand.NextDouble() > .5)
+                v *= -1;
+            return v;
+        }
+
+        public void AddRandomBalls(int count = 10)
+        {
+            float minSpeed = .5f;
+            float maxSpeed = 5;
+            float radius = 10;
+            Random rand = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                Balls.Add(
+                    new Ball(
+                        x: Width * (float)rand.NextDouble(),
+                        y: Height * (float)rand.NextDouble(),
+                        xVel: RandomVelocity(rand, minSpeed, maxSpeed),
+                        yVel: RandomVelocity(rand, minSpeed, maxSpeed),
+                        radius: radius,
+                        color: string.Format("#{0:X6}", rand.Next(0xFFFFFF))
+                    )
+                );
+            }
+        }
+    }
+}
