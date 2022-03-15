5dde121b766531791d48156ec6486b580814936b
Ralf1108 <ralf.kornelius@gmx.de>
2022-03-15 22:31:19 +0100

    implemented export to pdf

---
+++ BlazorSkiaWebAssemblyPerformanceTester/BlazorSkiaWebAssemblyPerformanceTester/DownloadHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorSkiaWebAssemblyPerformanceTester
+{
+    public class DownloadHelper
+    {
+        public static async Task SaveAs(IJSRuntime js, string filename, byte[] data)
+        {
+            await js.InvokeAsync<object>(
+                "window.downloadFile",
+                filename,
+                Convert.ToBase64String(data));
+        }
+    }
+}
